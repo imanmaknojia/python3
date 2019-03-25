46f12f556cd83e743b520d7e91286c660af683cf
imanmaknojia <47881224+imanmaknojia@users.noreply.github.com>
2019-03-25 10:06:09 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,9 @@
+def function1():
+	n=int(input("Enter a number: "))
+	if n>0:
+		print("the number is a positive number")
+	elif n<0:
+		print("the number is negative")
+	elif n==0:
+		print("the number is zero")
+function1()
